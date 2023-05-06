453b222ff692ff89d11abe38537f2523455ef816
“marrullas” <marrullas@gmail.com>
2023-05-05 22:01:11 -0500

    app toktik

---
+++ toktik/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:toktik/presentation/providers/discover_provider.dart';
+import 'package:toktik/presentation/screens/discover/discover_screen.dart';
+
+import 'config/theme/app_theme.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      
+      providers: [
+        ChangeNotifierProvider(
+          lazy: false, //propiedad opcional para que no se cargue hasta que se necesite, por defecto es true
+          create: (_) => DiscoverProvider()..loadNextPage()),
+      ],
+      child: MaterialApp(
+        title: 'TokTik',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme().getTheme(),
+        home: const DiscoverScreen()
+      ),
+    );
+  }
+}
---
+++ toktik/lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
+import 'package:toktik/presentation/widgets/video/full_screen_player.dart';
+
+class VideoScrollableView extends StatelessWidget {
+  const VideoScrollableView({
+    super.key,
+    required this.videos,
+  });
+  final List<VideoPost> videos;
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+        scrollDirection: Axis.vertical,
+        physics: const BouncingScrollPhysics(),
+        itemCount: videos.length,
+        itemBuilder: (context, index) {
+          final VideoPost videoPost = videos[index];
+
+          return Stack(
+            children: [
+
+              FullScreenPlayer(videoUrl: videoPost.videoUrl, caption: videoPost.caption),
+              Positioned(
+                bottom: 40,
+                right: 20,
+                child: VideoButtons(video: videoPost))
+              ],
+          );
+        });
+  }
+}
